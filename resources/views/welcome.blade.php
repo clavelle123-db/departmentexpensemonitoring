7b8c81b3223f9a0837735e83277f49ee442ad17c
rona <ronajeanumbao03@gmai.com>
2025-11-24 13:10:29 +0800

    as

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@ asdas
     <!-- Main container -->
     <div class="flex min-h-screen items-center justify-center px-6 sm:px-12">
         <div class="text-center bg-white bg-opacity-80 p-10 rounded-xl shadow-2xl w-full max-w-lg">
-
+hhashdhashdasd
             <!-- Logo -->
             <div class="mb-6">
                 <img src="{{ asset('images/melogo.png') }}" alt="Logo" class="mx-auto w-30 h-20 mb-4">
