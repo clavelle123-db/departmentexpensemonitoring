e2b36ee1dc07d76436ff7740562ab884c87b00dc
rona <ronajeanumbao03@gmai.com>
2025-11-26 11:01:36 +0800

    As

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -3,11 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
 
 class Section extends Model {
     protected $primaryKey = 'section_id';
-    protected $fillable = ['section_name', 'year_level', 'no_of_students', 'treasurer_id'];
+    protected $fillable = ['section_name', 'year_level','no_of_students'];
 
     public function events() {
         return $this->hasMany(Event::class, 'applied_to');
@@ -15,11 +14,6 @@ class Section extends Model {
 
     public function treasurers() {
         return $this->hasMany(Treasurer::class, 'section_assigned');
-    }
-
-    // Add this relationship to fetch the user assigned as treasurer
-    public function treasurer() {
-        return $this->belongsTo(User::class, 'treasurer_id', 'id');
     }
 
     public function user() {
